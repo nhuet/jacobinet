Fix padding and gradient handling in MaxPooling2D and AveragePooling2D with 'same' padding
- Fixed gradient computation in MaxPooling2D when combining max operator with first linear components.
- Replaced ZeroPadding with ConstantPadding (using -infinity, approximated by -1e16) in MaxPooling2D.
- Created custom 'ConstantPadding' layer (not natively supported in Keras).
- Refined 'BackwardMaxPooling' to properly handle the chain rule and rewritten 'backward_max' to use one-hot encoding.
- Updated AveragePooling2D to properly capture gradients with 'reflect' padding mode.
- Added unit tests ('test_backward_model') and extended 'compute_backward' for improved debugging.

jacobinet/layers/pooling/utils_conv.py
@@ -4,15 +4,30 @@ import keras.ops as K
 
 import numpy as np
 
-from typing import Dict
+from typing import Dict, List
 
 
-def get_in_channels(layer) -> int:
+def get_in_channels(data_format, input_dim_wo_shape) -> int:
     in_channels: int
+    """
+    try:
+        layer.input
+    except AttributeError:
+        import pdb; pdb.set_trace()
+    """
+
+    """
     if layer.data_format == "channels_last":
         in_channels = layer.input.shape[-1]
     else:
         in_channels = layer.input.shape[1]
+
+    return in_channels
+    """
+    if data_format == "channels_last":
+        in_channels = input_dim_wo_shape.shape[-1]
+    else:
+        in_channels = input_dim_wo_shape[0]
 
     return in_channels
 
@@ -48,17 +63,19 @@ def get_conv_op_config(config: Dict, in_channels: int) -> keras.Variable:
     return keras.Variable(kernel_pool, trainable=False)
 
 
-def get_conv_op(layer: MaxPooling2D) -> DepthwiseConv2D:
+def get_conv_op(layer: MaxPooling2D, input_dim_wo_batch:List[int]) -> DepthwiseConv2D:
 
     config: Dict = layer.get_config()
-    in_channels = get_in_channels(layer)
+    #in_channels = get_in_channels(layer)
+    in_channels:int = get_in_channels(data_format=layer.data_format, input_dim_wo_shape=input_dim_wo_batch)
     kernel: keras.Variable = get_conv_op_config(config, in_channels)
 
     # define convolution
     filters: int = np.prod(config["pool_size"])
     pool_size: tuple[int] = config["pool_size"]
     strides: tuple[int] = config["strides"]
-    padding: str = config["padding"]
+    #padding: str = config["padding"]
+    padding:str = "valid"
     data_format: str = config["data_format"]
 
     layer_conv: DepthwiseConv2D = DepthwiseConv2D(

tests/conftest.py
@@ -125,9 +125,18 @@ def compute_backward_layer(
 
 
 def compute_backward_model(
-    input_shape, model, backward_model, index_output=0, grad_value=None
+    input_shape, model, backward_model, index_output=0, grad_value=None, value="rand"
 ):
-    input_ = torch.randn(np.prod(input_shape), requires_grad=True)
+    if value =="rand":
+        input_np = np.asarray(100.*np.random.rand(np.prod(input_shape)) - 50., dtype='float32')
+    elif value== "zeros":
+        input_np = np.asarray(0.*np.random.rand(np.prod(input_shape)) - 0., dtype='float32')
+    elif value=="ones":
+        input_np = np.asarray(0.*np.random.rand(np.prod(input_shape)) + 1., dtype='float32')
+    else:
+        raise ValueError('unknown value {}'.format(value))
+    #input_ = torch.randn(np.prod(input_shape), requires_grad=True)
+    input_ = torch.tensor(input_np, requires_grad=True)
     input_reshape = torch.reshape(input_, [1] + list(input_shape))
     output = model(input_reshape)
     select_output = output[0, index_output]

tests/test_backward_models.py
@@ -8,6 +8,7 @@ from keras.layers import (
     Conv2D,
     DepthwiseConv2D,
     AveragePooling2D,
+    MaxPooling2D, 
     Input,
 )
 from keras.models import Sequential, Model
@@ -60,17 +61,27 @@ def test_sequential_multiD():
     compute_backward_model((input_dim,), model, backward_model)
     serialize_model([input_dim, 1], backward_model)
 
-@pytest.mark.parametrize("padding", ["valid", "same"])
-def test_sequential_multiD_pooling(padding):
 
-    input_dim = 36
-    layers = [Reshape((1, 6, 6)), AveragePooling2D((2, 2), (1, 1), padding=padding), ReLU(), Reshape((-1,)), Dense(1)]
+@pytest.mark.parametrize("padding, layer_name", [("valid", "average"), ("same","average"), ("valid", "max"), ("same","max") ])
+def test_sequential_multiD_pooling(padding, layer_name):
+
+    input_shape = (1, 3, 6)
+    input_dim = np.prod(input_shape)
+    layer_ = None
+    if layer_name=="average":
+        layer_ = AveragePooling2D((2, 2), (1, 1), padding=padding)
+    if layer_name =="max":
+        layer_ = MaxPooling2D((2, 2), (1, 2), padding=padding)
+    layers = [Reshape(input_shape), layer_, ReLU(), Reshape((-1,)), Dense(1)]
     model = Sequential(layers)
     _ = model(torch.ones((1, input_dim)))
+
     backward_model = clone_to_backward(model)
     # model is not linear
     _ = backward_model([torch.ones((1, input_dim)), torch.ones((1,1))])
-    compute_backward_model((input_dim,), model, backward_model)
+    compute_backward_model((input_dim,), model, backward_model, value="rand")
+    compute_backward_model((input_dim,), model, backward_model, value="zeros")
+    compute_backward_model((input_dim,), model, backward_model, value="ones")
     serialize_model([input_dim, 1], backward_model)
 
 