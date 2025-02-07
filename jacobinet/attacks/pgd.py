a275af5d8a1989aa82add4600d23ce386a94c474
ducoffeM <melanie.ducoffe@airbus.com>
2025-02-07 09:57:57 +0100

    Add hyperparameter to PGD: Enable input boundary clipping.

--- jacobinet/attacks/pgd.py
+++ jacobinet/attacks/pgd.py
@@ -29,9 +29,13 @@ class PGD_Cell(keras.Layer):
 
     def call(self, y, states):
         x = states[0] # x
+        x_init = states[1]
         # get adversarial attack using fgsm_model
         adv_x = self.fgsm_model([x, y])
-        return adv_x, [adv_x]
+        # clip projected sample hard coding for now
+        adv_x = K.maximum(adv_x, x_init - 0.31)
+        adv_x = K.minimum(adv_x, x_init + 0.31)
+        return adv_x, [adv_x, x_init]
 
 
 class ProjectedGradientDescent(AdvLayer):
@@ -47,13 +51,15 @@ class ProjectedGradientDescent(AdvLayer):
         self.n_iter = n_iter
         self.fgsm_layer = fgsm_model.layer_adv
         pgd_cell = PGD_Cell(fgsm_model=fgsm_model)
-        self.inner_layer = RNN(pgd_cell)
+        self.inner_layer = RNN(pgd_cell, return_sequences=True)
 
         # init attributes with fgsm_layer
         self.upper = self.fgsm_layer.upper
         self.lower = self.fgsm_layer.lower
         self.p = self.fgsm_layer.p
         self.radius = self.fgsm_layer.radius
+
+        self.random_init = True
 
     # saving
     def get_config(self):
@@ -85,7 +91,19 @@ class ProjectedGradientDescent(AdvLayer):
         x, y = inputs
         z = keras.ops.repeat(keras.ops.expand_dims(y, 1), self.n_iter, 1)
 
-        return self.inner_layer(z, initial_state=[x])
+        if self.random_init:
+            # start with a noisy version around x
+            input_shape_wo_batch = x.shape[1:]
+            noise = keras.random.uniform(input_shape_wo_batch, \
+                                         minval=-0.3, \
+                                         maxval=0.3, dtype=None, seed=None)
+            x_0 = x + K.expand_dims(noise, 0)
+            # clip
+            x_0 = K.maximum(x_0, x-0.3)
+            x_0 = K.minimum(x_0, x+0.3)
+        else:
+            x_0 = x
+        return self.inner_layer(z, initial_state=[x_0, x])
 
 
 def get_pgd_model(model, 
@@ -105,8 +123,23 @@ def get_pgd_model(model,
     pgd_layer = ProjectedGradientDescent(n_iter=n_iter, fgsm_model=fgsm_model)
     inputs:List[Tensor] = to_list(fgsm_model.inputs)
 
-    output = pgd_layer(inputs)
+    output_adv = pgd_layer(inputs)
+    # filter with the most adversarial example
+    input_shape_wo_batch = list(inputs[0].shape[1:])
+    pred_adv = K.reshape(output_adv, [-1]+input_shape_wo_batch)
+    y_adv = model(pred_adv)
+    n_class = y_adv.shape[-1]
+    y_gt = K.repeat(K.expand_dims(inputs[1], 1), n_iter, 1) # (batch, n_iter, n_class)
+    y_gt = K.reshape(y_gt, [-1, n_class])
+    # compute cross entropy
+    loss_adv = K.reshape(K.categorical_crossentropy(y_gt, y_adv, from_logits=True), [-1, n_iter])
+    index_adv = K.argmax(loss_adv, -1)[:,None] # (batch, 1)
+    mask = K.one_hot(index_adv, n_iter) # (batch, n_iter)
+    mask = K.reshape(mask, [-1, n_iter]+[1]*len(input_shape_wo_batch))
 
+    output= K.sum(mask*output_adv, 1)
+
+    
     pgd_model = AdvModel(inputs=inputs,
                           outputs=output,
                           layer_adv= pgd_layer,
